c963b1e7258c35cf3fd006fac147f09a3d9023e4
liviafviana <liviafvianaa@gmail.com>
2025-11-05 16:46:48 -0300

    Update Hospede.h

--- include/Hospede.h
+++ include/Hospede.h
@@ -1,19 +1,1 @@
-#ifndef HOSPEDE_H
-#define HOSPEDE_H
 
-#include <string>
-
-
-class Hospede {
-private:
-    std::string nome; 
-    std::string cpf;  
-
-public:
-   
-    Hospede(std::string nome, std::string cpf);
-    std::string getNome() const;
-    std::string getCpf() const;
-};
-
-#endif
