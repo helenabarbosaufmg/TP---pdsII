e072303f0aacf2e927efee82b89641c78da11489
liviafviana <liviafvianaa@gmail.com>
2025-11-05 16:45:03 -0300

    Hospede.h e .cpp

--- include/Hospede.h
+++ include/Hospede.h
@@ -1,1 +1,34 @@
- 
+#ifndef HOSPEDE_H
+#define HOSPEDE_H
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+class Hospede {
+private:
+    string nome;
+    string rg;
+    string telefone;
+    string email;
+    int codigo;
+
+public:
+    Hospede();
+    Hospede(int codigo, const string& nome, const string& rg, const string& telefone, const string& email);
+
+    int HCodigo() const;
+    string HNome() const;
+    string HRG() const;
+    string HTelefone() const;
+    string HEmail() const;
+
+    void H_Nome(const string& nome);
+    void H_RG(const string& rg);
+    void H_Telefone(const string& telefone);
+    void H_Email(const string& email);
+
+    void Hospede_info() const;
+};
+
+#endif
--- src/Hospede.cpp
+++ src/Hospede.cpp
@@ -1,1 +1,63 @@
+#include "Hospede.h"
 #include <iostream>
+using namespace std;
+
+Hospede::Hospede() {
+    codigo = 0;
+    nome = "";
+    rg = "";
+    telefone = "";
+    email = "";
+} //hospede vazio
+
+Hospede::Hospede(int codigo, const string& nome, const string& rg, const string& telefone, const string& email) {
+    this->codigo = codigo;
+    this->nome = nome;
+    this->rg = rg;
+    this->telefone = telefone;
+    this->email = email;
+}
+
+int Hospede::HCodigo() const {
+    return codigo;
+}
+
+string Hospede::HNome() const {
+    return nome;
+}
+
+string Hospede::HRG() const {
+    return rg;
+}
+
+string Hospede::HTelefone() const {
+    return telefone;
+}
+
+string Hospede::HEmail() const {
+    return email;
+}
+
+void Hospede::H_Nome(const string& nome) {
+    this->nome = nome;
+}
+
+void Hospede::H_RG(const string& rg) {
+    this->rg = rg;
+}
+
+void Hospede::H_Telefone(const string& telefone) {
+    this->telefone = telefone;
+}
+
+void Hospede::H_Email(const string& email) {
+    this->email = email;
+}
+
+void Hospede::Hospede_info() const {
+    cout << "Código do Hospede: " << codigo << endl;
+    cout << "Nome: " << nome << endl;
+    cout << "RG: " << rg << endl;
+    cout << "Telefone: " << telefone << endl;
+    cout << "Email: " << email << endl;
+}
