14293cc876366f5b1eb49fc3a01bfdbab3f139c8
Lilla Xavier <lilladelucas25@gmail.com>
2025-11-08 16:12:40 -0300

    Update Quartos.h

--- include/Quartos.h
+++ include/Quartos.h
@@ -5,23 +5,24 @@
 
 class Quartos {
 private:
-    int numero;           
-    std::string tipo;     
-    double preco;         
-    bool ocupado;         
+    int numero;
+    std::string tipo;
+    double preco;
+    bool ocupado;
 
 public:
-    
+    // 🔹 Construtor padrão (resolve seu erro)
+    Quartos() : numero(0), tipo("Indefinido"), preco(0.0), ocupado(false) {}
+
+    // 🔹 Construtor completo (o que já existia)
     Quartos(int numero, std::string tipo, double preco);
 
     int QNumero() const;
     std::string QTipo() const;
     double QPreco() const;
     bool estaOcupado() const;
-
     void ocupar();
     void liberar();
 };
 
-#endif
- 
+ #endif
